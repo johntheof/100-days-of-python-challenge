c8bd1d0f38ee6cec9573ba3cb602c9424b14e2bf
John Theofanopoulos <41819447+johntheof@users.noreply.github.com>
2022-07-12 11:13:10 +0300

    Finalised data type examples

--- Day 2 - Challenges/data types examples.py
+++ Day 2 - Challenges/data types examples.py
@@ -1,4 +1,48 @@
 # Data Types
 
-#string
+#STRINGS
+#------------
+#Get the first character out of a string // This is called 'Subscripting'.
+print('Hello'[0])
 
+#Get the last character exercise out of a string
+print('Hello'[-1])
+print('Hello'[4])
+
+#Adding strings together // CONCUTINATING
+print('123' + '345')
+
+#------------
+
+#INTEGERS 
+#------------
+#Simple Calculations
+print(123+345)
+
+#Underscores are ignored on numbers
+print(123_456_789)
+
+#------------
+
+#Floats // When a number has decimal points
+#------------
+print(3141.59) 
+
+#------------
+
+#Boolean // True or False
+#------------
+
+print(True)
+print(False)
+
+#Boolean operations and results
+print(True or False) #True or False = True
+print(True or True) #True or True = True
+print(False or False) #False or False = False
+
+print(True and True) #True and True = True
+print(True and False) #True and False = False
+print(False and False) #False and False = False
+
+#------------
