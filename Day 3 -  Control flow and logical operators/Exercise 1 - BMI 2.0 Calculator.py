14e9677b979aa184e77547c824baad25c4167375
John Theofanopoulos <41819447+johntheof@users.noreply.github.com>
2022-07-13 19:40:17 +0300

    small fix

--- Day 3 -  Control flow and logical operators/Exercise 1 - BMI 2.0 Calculator.py
+++ Day 3 -  Control flow and logical operators/Exercise 1 - BMI 2.0 Calculator.py
@@ -21,4 +21,4 @@ elif bmi < 30:
 elif bmi < 35:
     print(f'Your BMI is {bmi}, you are obese.')
 else:
-    print(f'Your BMI is {BMI}, you are clinically obese.')
+    print(f'Your BMI is {bmi}, you are clinically obese.')
