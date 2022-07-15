5eaa38d76fdf9cc2755145880266d18d59dd984d
John Theofanopoulos <41819447+johntheof@users.noreply.github.com>
2022-07-15 10:03:21 +0300

    commit solution to exercise

--- Day 4 - Randomisation and python lists/Exercise 4 - Rock papers scissors.py
+++ Day 4 - Randomisation and python lists/Exercise 4 - Rock papers scissors.py
@@ -1,5 +1,5 @@
 import random
-option = input('What do you choose? Type 0 for Rock, 1 for Paper and 2 for scissors.')
+option = int(input('What do you choose? Type 0 for Rock, 1 for Paper and 2 for scissors. '))
 rock = ("""
     _______
 ---'   ____)
@@ -27,6 +27,22 @@ scissors = ("""
 ---.__(___)
 """)
 
-computer_select = random.randint(2)
-print(computer_select)
+computer_select = random.randint(0,2)
+list = [rock,paper,scissors]
 
+print(f'You have selected {list[option]}')
+
+print(f'Computer has selected {list[computer_select]}')
+
+if option >= 3 or option <0:
+    print('You typed an invalid number, you lose!')
+elif option == 0 and computer_select == 2:
+    print('You win')
+elif computer_select ==0 and option == 2:
+    print('You lose!')
+elif computer_select > option:
+    print('You lose.')
+elif option > computer_select:
+    print('You win.')
+elif option == computer_select:
+    print('It\' a draw.')
