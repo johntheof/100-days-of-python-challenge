44c45a4f0c8e9a523e91176617aa6ee90be5c1bf
johntheof <master@johntheof.com>
2022-07-16 14:53:20 +0300

    commit solution

--- Day 5 - Python Loops/Exercise 5 - Create a password generator.py
+++ Day 5 - Python Loops/Exercise 5 - Create a password generator.py
@@ -1,17 +1,24 @@
-#create a password generator
-#user will input how many letters user wants to have in password
+# #create a password generator
+# #user will input how many letters user wants to have in password
 import random
 print('Welcome to the password generator.')
-password = int(input('How many letters you want to have in your password? '))
-symbols = input('How many symbols? ')
-numbers = input('How many numbers? ')
+input_letters = int(input('How many letters you want to have in your password? '))
+input_symbols = int(input('How many symbols? '))
+input_numbers = int(input('How many numbers? '))
 
-letters = ['a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z']
-symbols = ['!,#,$,%,^,&,*,(,),_,+']
-numbers = ['0,1,2,3,4,5,6,7,8,9']
+letters = ['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','k','K','l','L','m','M','n','N','p','P','o','O','y','Y','u','U','x','X','z','Z','q','Q','w','W']
+symbols = ['!','#','$','%','^','&','*','(',')','_','+']
+numbers = ['0','1','2','3','4','5','6','7','8','9']
 
-finalpass = ''
-for char in range(1,password + 1):
-    random_letters = random.choice(letters)
-    finalpass += random_letters
-print(finalpass)
+password = []
+for char in range(1, input_letters + 1):
+    password.append(random.choice(letters))
+for char in range(1, input_symbols + 1):
+    password += random.choice(symbols)
+for char in range(1, input_numbers + 1):
+    password += random.choice(numbers)
+random.shuffle(password)
+passle = ''
+for char in password:
+    passle += char
+print(f'Your password is {passle} .')
